ddbc765da34c432cb4e2597a299974965fdc08ad
luka-lta <luka@luka-lta.dev>
2026-05-01 23:10:25 +0200

    Initial Commit

---
+++ habbit-tracker/habbit_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  habbit_trackerApp.swift
+//  habbit-tracker
+//
+//  Created by Luka on 01.05.26.
+//
+
+import SwiftUI
+
+@main
+struct habbit_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
